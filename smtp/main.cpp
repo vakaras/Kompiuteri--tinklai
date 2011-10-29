02fd9415f2853092fb9d6fafdecb1ea61c3e7d28
Vytautas Astrauskas <vastrauskas@gmail.com>
2011-10-29 16:04:34 +0300

    Klaidų pataisymai.

--- smtp/main.cpp
+++ smtp/main.cpp
@@ -21,19 +21,28 @@ int main(int argc, const char *argv[]) {
     *sender_server  = (argc > 5 ? argv[5] : "astrauskas.lt"),
     *html_file      = (argc > 6 ? argv[6] : "mail.html"),
     *subject        = (argc > 7 ? argv[7] : "Testas 01.");
-  int post = (argc > 3 ? atoi(argv[3]) : 25);
+  int port = (argc > 3 ? atoi(argv[3]) : 25);
 
-  MIMEHTML html("mail.html");
+  printf("Argumentų kiekis:   %d\n", argc);
+  printf("Gavėjas:            %s\n", recipient);
+  printf("Serveris:           %s\n", server);
+  printf("Prievadas:          %d\n", port);
+  printf("Siuntėjas:          %s\n", author);
+  printf("Siunėjo serveris:   %s\n", sender_server);
+  printf("HTML failas:        %s\n", html_file);
+  printf("Tema:               %s\n", subject);
+
+  MIMEHTML html(html_file);
   MIMEMultipart msg(subject, recipient);
   msg.append(&html);
-  for (int i = 7; i < argc; i++) {
+  for (int i = 8; i < argc; i++) {
     MIMEImage *img = new MIMEImage(
         argv[i], argv[i] + string("@astrauskas.lt"));
     msg.append(img);
     }
 
-  send_message("vastrauskas@gmail.com", "localhost", 25,
-      "vytautas@astrauskas.lt", "astrauskas.lt", msg);
+  send_message(recipient, server, port, author, sender_server, msg);
+
 
   return 0;
   }
