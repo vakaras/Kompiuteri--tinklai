66520ca1337c89fc8d9852bc0097128132299753
Vytautas Astrauskas <vastrauskas@gmail.com>
2011-10-29 15:50:52 +0300

    Pridėti argumentai.

--- smtp/main.cpp
+++ smtp/main.cpp
@@ -9,26 +9,28 @@ using namespace smtp::encoders;
 using namespace smtp::utils;
 using namespace smtp::mime;
 using namespace smtp::sender;
-
-
-#include <string>
-#include <cassert>
-#include <limits>
-#include <stdexcept>
-#include <cctype>
+using namespace std;
 
 
 int main(int argc, const char *argv[]) {
 
+  const char
+    *recipient      = (argc > 1 ? argv[1] : "vastrauskas@gmail.com"),
+    *server         = (argc > 2 ? argv[2] : "localhost"),
+    *author         = (argc > 4 ? argv[4] : "vytautas@astrauskas.lt"),
+    *sender_server  = (argc > 5 ? argv[5] : "astrauskas.lt"),
+    *html_file      = (argc > 6 ? argv[6] : "mail.html"),
+    *subject        = (argc > 7 ? argv[7] : "Testas 01.");
+  int post = (argc > 3 ? atoi(argv[3]) : 25);
 
   MIMEHTML html("mail.html");
-  MIMEImage img("joana.jpeg", "joana@astrauskas.lt");
-  MIMEMultipart msg(
-      "Testas 01: Lietuviškos raidės.", "vastrauskas@gmail.com");
+  MIMEMultipart msg(subject, recipient);
   msg.append(&html);
-  msg.append(&img);
-
-  //msg.write(stdout);
+  for (int i = 7; i < argc; i++) {
+    MIMEImage *img = new MIMEImage(
+        argv[i], argv[i] + string("@astrauskas.lt"));
+    msg.append(img);
+    }
 
   send_message("vastrauskas@gmail.com", "localhost", 25,
       "vytautas@astrauskas.lt", "astrauskas.lt", msg);
