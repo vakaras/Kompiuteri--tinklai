2f92ce8946032c213f6efd218dfdb26bb25e92d7
Vytautas Astrauskas <vastrauskas@gmail.com>
2011-10-29 14:46:21 +0300

    Pataisyta klaidelė.

--- smtp/sender.cpp
+++ smtp/sender.cpp
@@ -5,6 +5,14 @@ using namespace dict::socket;
 using namespace std;
 
 
+int get_code(const char *buffer) {
+  return 
+    (buffer[0] - '0') * 100 +
+    (buffer[1] - '0') * 10 +
+    (buffer[2] - '0');
+  }
+
+
 void smtp::sender::send_message(
     string recipient, string server, int port,
     string author, string sender_server, 
@@ -13,6 +21,13 @@ void smtp::sender::send_message(
   ClientSocket socket(server.c_str(), port);
 
   char buffer[MAX_CHAR];
+  socket.read_block((void *) buffer, 1, MAX_CHAR);
+  printf("Received: %s\n", buffer);
+  if (get_code(buffer) != 220) {
+    throw BaseException(
+        (string("Wrong return code. Expected: 220. Message: ") +
+        string(buffer)).c_str());
+    }
 
   // Pasisveikinimas.
   string hello = "ehlo " + sender_server;
@@ -20,7 +35,7 @@ void smtp::sender::send_message(
   while (true) {
     socket.read_block((void *) buffer, 1, MAX_CHAR);
     printf("Received: %s\n", buffer);
-    if (!(buffer[0] == '2' && buffer[1] == '5' && buffer[2] == '0')) {
+    if (get_code(buffer) != 250) {
       throw BaseException(
           (string("Wrong return code. Expected: 250. Message: ") +
           string(buffer)).c_str());
