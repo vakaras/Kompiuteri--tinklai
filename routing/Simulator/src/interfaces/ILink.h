d7a491dd386f82039c8048ae3506557885b15782
Vytautas Astrauskas <vastrauskas@gmail.com>
2011-12-30 15:45:33 +0200

    Klaidelės pataisymas ILink sąsajoje.

--- routing/Simulator/src/interfaces/ILink.h
+++ routing/Simulator/src/interfaces/ILink.h
@@ -12,7 +12,7 @@ class ILink
     /** Sends len bytes.
       @returns true if successfully send.
       */
-    _V bool write(const Byte *bytes, int len) = 0;
+    _V bool write(const Byte *bytes, uint len) = 0;
 
     /** Receives no more than maxlen bytes.
       @param time if there is no data, returns after;
@@ -21,7 +21,7 @@ class ILink
     _V int  read(Byte *bytes, uint maxlen, ulong time=ULONG_MAX) = 0;
 
     /** Resets to initial state. */
-    _V void reset();
+    _V void reset() = 0;
 };
 
 #endif // ILINK_H
